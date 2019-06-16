05e246c8e7f7ae8b58eea696c31c8028b0ac5da5
esnap <sepanse@gmail.com>
2019-06-16 23:55:32 +0200

    feat: db integration

--- src/main/java/com/sp/demoservice/domain/DomainItem.java
+++ src/main/java/com/sp/demoservice/domain/DomainItem.java
@@ -5,19 +5,27 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.NonNull;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Getter
 @AllArgsConstructor
 @Data
 @Entity
 
+@Table(name="DomainItem")
 public class DomainItem {
 
+    private static final long serialVersionUID = -3009157732242241606L;
+
     @NonNull
+    @Id
     private String id;
 
     @NonNull
+    @Column(name="information")
     private String information;
 
 }
