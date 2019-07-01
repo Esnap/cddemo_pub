c413ebe5ad67310cb07c9aa64e9c549cd30fe15a
esnap <sepanse@gmail.com>
2019-07-02 00:47:00 +0200

    chore: remove unused class

--- src/main/java/com/sp/demoservice/domain/repository/DomainItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sp.demoservice.domain.repository;
-
-import com.sp.demoservice.domain.DomainItem;
-import org.springframework.data.repository.CrudRepository;
-
-public interface DomainItemRepository extends CrudRepository <DomainItem, Long> {
-}
